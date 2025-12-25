ce57c110e4562e564ec79c0670aba1e9426901f8
Masayuki Sugahara <equaaqua@hotmail.com>
2025-12-25 17:16:04 +0000

    fix: check Sanctrum existence

--- app/laravel/config/sanctum.php
+++ app/laravel/config/sanctum.php
@@ -1,7 +1,5 @@
 <?php
 
-use Laravel\Sanctum\Sanctum;
-
 return [
 
     /*
@@ -18,7 +16,7 @@ return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
+        class_exists(\Laravel\Sanctum\Sanctum::class) ? \Laravel\Sanctum\Sanctum::currentApplicationUrlWithPort() : ''
     ))),
 
     /*
